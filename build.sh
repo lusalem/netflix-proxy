30e7a549e0cc9c7ee5192cabef24d12658b1d1b1
Salem <39432507+lusalem@users.noreply.github.com>
2020-06-28 03:06:29 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -174,8 +174,45 @@ sudo iptables -t nat -A PREROUTING -i ${IFACE} -p tcp --dport 80 -j REDIRECT --t
   && sudo iptables -A INPUT -i lo -j ACCEPT\
   && sudo iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT\
   && sudo iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT\
-  && sudo iptables -A INPUT -p udp -m udp --dport 1:65535 -j ACCEPT\
-  && sudo iptables -A INPUT -p tcp -m tcp --dport 1:65535 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m udp --dport 21 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m udp --dport 53 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m udp --dport 5353 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m udp --dport 5201 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m udp --dport 9000 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m udp --dport 621 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m udp --dport 622 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m udp --dport 10000 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m udp --dport 11000 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m udp --dport 12000 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m udp --dport 13000 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m udp --dport 14000 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m udp --dport 15000 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m udp --dport 16000 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m udp --dport 17000 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m udp --dport 18000 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m udp --dport 19000 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m udp --dport 20000 -j ACCEPT\
+  && sudo iptables -A INPUT -p udp -m udp --dport 65000 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m tcp --dport 21 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m tcp --dport 8080 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m tcp --dport 5201 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m tcp --dport 9000 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m tcp --dport 621 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m tcp --dport 622 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m tcp --dport 10000 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m tcp --dport 11000 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m tcp --dport 12000 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m tcp --dport 13000 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m tcp --dport 14000 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m tcp --dport 15000 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m tcp --dport 16000 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m tcp --dport 17000 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m tcp --dport 18000 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m tcp --dport 19000 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m tcp --dport 20000 -j ACCEPT\
+  && sudo iptables -A INPUT -p tcp -m tcp --dport 65000 -j ACCEPT\
   && sudo iptables -A INPUT -j REJECT --reject-with icmp-host-prohibited
 log_action_end_msg $?
 
@@ -188,8 +225,11 @@ sudo ip6tables -t nat -A PREROUTING -i ${IFACE} -p tcp --dport 80 -j REDIRECT --
   && sudo ip6tables -A INPUT -i lo -j ACCEPT\
   && sudo ip6tables -A INPUT -p tcp -m state --state NEW -m tcp --dport 22 -j ACCEPT\
   && sudo ip6tables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT\
-  && sudo ip6tables -A INPUT -p udp -m udp --dport 1:65535 -j ACCEPT\
-  && sudo ip6tables -A INPUT -p tcp -m tcp --dport 1:65535 -j ACCEPT\
+  && sudo ip6tables -A INPUT -p udp -m udp --dport 53 -j ACCEPT\
+  && sudo ip6tables -A INPUT -p udp -m udp --dport 5353 -j ACCEPT\
+  && sudo ip6tables -A INPUT -p tcp -m tcp --dport 80 -j ACCEPT\
+  && sudo ip6tables -A INPUT -p tcp -m tcp --dport 8080 -j ACCEPT\
+  && sudo ip6tables -A INPUT -p tcp -m tcp --dport 443 -j ACCEPT\
   && sudo ip6tables -A INPUT -j REJECT --reject-with icmp6-adm-prohibited
 log_action_end_msg $?
 
